8da88f71b1aa623e1efe470b6d466305211db77f
Mariam Babutsidze <mariambabutsidze@gmail.com>
2024-02-08 18:25:12 +0400

    Initial Commit

---
+++ Parks/ParksApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParksApp.swift
+//  Parks
+//
+//  Created by Mariam Babutsidze on 08.02.24.
+//
+
+import SwiftUI
+
+@main
+struct ParksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
